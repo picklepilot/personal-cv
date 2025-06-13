e8739522ab9128042fa876c0769a227c5ece81fd
picklepilot <kaczmar.justin@gmail.com>
2025-06-13 10:20:02 -0500

    refactor: optimize images and use next image component

--- app/components/PhotoGrid.tsx
+++ app/components/PhotoGrid.tsx
@@ -1,28 +1,29 @@
 'use client'
 
+import Image from 'next/image'
 import { useState } from 'react'
 import Lightbox from 'yet-another-react-lightbox'
 import 'yet-another-react-lightbox/styles.css'
 
 const images = [
     {
-        src: '/life/jk_fq_japan.jpg',
+        src: '/life/jk_fq_japan.webp',
         alt: 'Justin and Franny Mount Fuji',
     },
     {
-        src: '/life/harvey_big_sky.jpg',
+        src: '/life/harvey_big_sky.webp',
         alt: 'Harvey Big Sky Montana',
     },
     {
-        src: '/life/jk_fq_chile.jpeg',
+        src: '/life/jk_fq_chile.webp',
         alt: 'Chile',
     },
     {
-        src: '/life/jk_fq_japan2.jpeg',
+        src: '/life/jk_fq_japan2.webp',
         alt: 'Justin and Franny Japan',
     },
     {
-        src: '/life/jk_fq_italy.jpeg',
+        src: '/life/jk_fq_italy.webp',
         alt: 'Justin and Franny Italy',
     },
 ]
@@ -34,50 +35,59 @@ export default function PhotoGrid() {
         <div className='relative z-10'>
             <div className='grid grid-cols-1 gap-4 sm:gap-5 md:grid-cols-4'>
                 <div className='relative col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
-                    <img
+                    <Image
                         src={images[0].src}
                         alt={images[0].alt}
-                        className='relative h-full w-full object-cover'
+                        fill
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='relative object-cover'
                         onClick={() => setIndex(0)}
                     />
                 </div>
-                <div className='col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
-                    <img
+                <div className='relative col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
+                    <Image
                         src={images[1].src}
                         alt={images[1].alt}
-                        className='h-full w-full object-cover'
+                        fill
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='object-cover'
                         onClick={() => setIndex(1)}
                     />
                 </div>
-                <div className='col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
-                    <img
+                <div className='relative col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
+                    <Image
                         src={images[2].src}
                         alt={images[2].alt}
-                        className='h-full w-full object-cover'
+                        fill
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='object-cover'
                         onClick={() => setIndex(2)}
                     />
                 </div>
-                <div className='col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
-                    <img
+                <div className='relative col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-2 md:h-[350px]'>
+                    <Image
                         src={images[3].src}
                         alt={images[3].alt}
-                        className='h-full w-full object-cover'
+                        fill
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='object-cover'
                         onClick={() => setIndex(3)}
                     />
                 </div>
-                <div className='col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-4 md:h-[350px]'>
-                    <img
+                <div className='relative col-span-1 h-[250px] cursor-pointer overflow-hidden rounded-xl sm:h-[300px] md:col-span-4 md:h-[350px]'>
+                    <Image
                         src={images[4].src}
                         alt={images[4].alt}
-                        className='h-full w-full object-cover'
+                        fill
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='object-cover'
                         onClick={() => setIndex(4)}
                     />
                 </div>
             </div>
             <div className='p-6 text-sm text-zinc-700 backdrop-blur-xs dark:text-zinc-300'>
-                That's my wife, Franny. We try to travel as much as possible. Getting to know me would be like getting
-                to know her considering many of my good qualities have come from her. And then there's our 8 year old
-                Wheaten, Harvey. He's definitely got... qualities.
+                That's my wife, Franny. We try to travel as much as possible. Also pictured is our 9 year old Wheaten,
+                Harvey. He's definitely got... qualities.
             </div>
 
             <Lightbox
